5b2bee0d65dbc973989cc112e61ffcee15d6f655
guyra <guyra>
2017-04-08 14:51:31 +0100

    added messages

---
+++ messages.js
@@ -0,0 +1,45 @@
+/**
+ * Created by osboxes on 08/04/17.
+ */
+message1 = {
+    text: ["line1","line2","line3","line4"],
+    image: ["message1_image1", "message1_image2"],
+    template: "template1.html",
+    days_to_show: ["monday", "wednesday"],
+    time_to_show: ["06:00", "12:00"],
+    date_to_show: ["01.01.2017", "01.01.2018"]};
+
+message2 = {
+    text: ["line1","line2","line3","line4","line5","line6","line7","line8","line9","line10"],
+    image: ["message2_image1"],
+    template: "template2.html",
+    days_to_show: ["tuesday", "wednesday"],
+    time_to_show: ["10:00", "16:00"],
+    date_to_show: ["01.03.2017", "01.05.2017"]
+};
+
+message3 = {
+    template: "template3.html",
+    days_to_show: ["sunday", "monday","tuesday", "wednesday", "thursday", "friday", "saturday"],
+    time_to_show: ["08:00", "22:00"],
+    date_to_show: ["01.05.2017", "16.06.2017"]
+};
+
+message4 = {
+    text: ["line1","line2"],
+    template: "template1.html",
+    days_to_show: ["monday"],
+    time_to_show: ["17:00", "19:00"],
+    date_to_show: ["29.03.2017", "16.04.2017"]
+};
+
+message5 = {
+    text: ["line1","line2","line3","line4","line5","line6","line7"],
+    image: ["message5_image1", "message5_image2"],
+    template: "template2.html",
+    days_to_show: ["monday","tuesday", "wednesday"],
+    time_to_show: ["01:00", "23:00"],
+    date_to_show: ["01.04.2017", "01.05.2017"]
+};
+
+messages = [message1, message2, message3, message4, message5];
