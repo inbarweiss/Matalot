d8d4c67cee85ead167acd6ef0fc00a40b777239e
guyra <guyra>
2017-04-11 16:25:59 +0100

    edits message parameters but doest work because of timing

--- messages.js
+++ messages.js
@@ -7,7 +7,10 @@ message1 = {
     template: "template1.html",
     days_to_show: ["monday", "wednesday"],
     time_to_show: ["06:00", "12:00"],
-    date_to_show: ["01.01.2017", "01.01.2018"]};
+    date_to_show: ["01.01.2017", "01.01.2018"],
+    ttl: 5000
+};
+
 
 message2 = {
     text: ["line1","line2","line3","line4","line5","line6","line7","line8","line9","line10"],
@@ -15,14 +18,16 @@ message2 = {
     template: "template2.html",
     days_to_show: ["tuesday", "wednesday"],
     time_to_show: ["10:00", "16:00"],
-    date_to_show: ["01.03.2017", "01.05.2017"]
+    date_to_show: ["01.03.2017", "01.05.2017"],
+    ttl: 5000
 };
 
 message3 = {
     template: "template3.html",
     days_to_show: ["sunday", "monday","tuesday", "wednesday", "thursday", "friday", "saturday"],
     time_to_show: ["08:00", "22:00"],
-    date_to_show: ["01.05.2017", "16.06.2017"]
+    date_to_show: ["01.05.2017", "16.06.2017"],
+    ttl: 5000
 };
 
 message4 = {
@@ -30,7 +35,8 @@ message4 = {
     template: "template1.html",
     days_to_show: ["monday"],
     time_to_show: ["17:00", "19:00"],
-    date_to_show: ["29.03.2017", "16.04.2017"]
+    date_to_show: ["29.03.2017", "16.04.2017"],
+    ttl: 5000
 };
 
 message5 = {
@@ -39,7 +45,8 @@ message5 = {
     template: "template2.html",
     days_to_show: ["monday","tuesday", "wednesday"],
     time_to_show: ["01:00", "23:00"],
-    date_to_show: ["01.04.2017", "01.05.2017"]
+    date_to_show: ["01.04.2017", "01.05.2017"],
+    ttl: 5000
 };
 
 messages = [message1, message2, message3, message4, message5];
