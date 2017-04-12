58352f6b374eef5fcf06c77ccebf366a860887bd
inbar weiss <inbar weiss>
2017-04-13 00:30:52 +0300

    la finito la comedia

--- messages.js
+++ messages.js
@@ -3,43 +3,50 @@
  */
 message1 = {
     text: ["line1","line2","line3","line4"],
-    image: ["message1_image1", "message1_image2"],
+    image: ["message1_image1.jpeg", "message1_image2.jpeg"],
     template: "template1.html",
-    days_to_show: ["monday", "wednesday"],
+    days_to_show: ["Monday", "Wednesday"],
     time_to_show: ["06:00", "12:00"],
-    date_to_show: ["01.01.2017", "01.01.2018"]};
+    date_to_show: ["01.01.2017", "01.01.2018"],
+    ttl: 5000
+};
+
 
 message2 = {
     text: ["line1","line2","line3","line4","line5","line6","line7","line8","line9","line10"],
-    image: ["message2_image1"],
+    image: ["message2_image1.jpeg"],
     template: "template2.html",
-    days_to_show: ["tuesday", "wednesday"],
+    days_to_show: ["Tuesday", "Wednesday"],
     time_to_show: ["10:00", "16:00"],
-    date_to_show: ["01.03.2017", "01.05.2017"]
+    date_to_show: ["01.03.2017", "01.05.2017"],
+    ttl: 5000
 };
 
 message3 = {
     template: "template3.html",
-    days_to_show: ["sunday", "monday","tuesday", "wednesday", "thursday", "friday", "saturday"],
+    days_to_show: ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
     time_to_show: ["08:00", "22:00"],
-    date_to_show: ["01.05.2017", "16.06.2017"]
+    date_to_show: ["01.05.2017", "16.06.2017"],
+    ttl: 5000
 };
 
 message4 = {
     text: ["line1","line2"],
     template: "template1.html",
-    days_to_show: ["monday"],
+    days_to_show: ["Monday"],
     time_to_show: ["17:00", "19:00"],
-    date_to_show: ["29.03.2017", "16.04.2017"]
+    date_to_show: ["29.03.2017", "16.04.2017"],
+    ttl: 5000
 };
 
 message5 = {
     text: ["line1","line2","line3","line4","line5","line6","line7"],
-    image: ["message5_image1", "message5_image2"],
+    image: ["message5_image1.jpeg", "message5_image2.jpg"],
     template: "template2.html",
-    days_to_show: ["monday","tuesday", "wednesday"],
+    days_to_show: ["Monday","Tuesday", "Wednesday"],
     time_to_show: ["01:00", "23:00"],
-    date_to_show: ["01.04.2017", "01.05.2017"]
+    date_to_show: ["01.04.2017", "01.05.2017"],
+    ttl: 5000
 };
 
 messages = [message1, message2, message3, message4, message5];
