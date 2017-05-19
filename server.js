77ee2d906b90b82aac4511149d3fcdf9c509914c
guyra <guyra>
2017-05-19 21:14:25 +0100

    finished

--- server.js
+++ server.js
@@ -10,72 +10,93 @@ var MongoClient = require('mongodb').MongoClient
 
 // Connection URL
 var url = 'mongodb://127.0.0.1:27017/myproject';
-noMessage = {
-    text: ["no message to show"],
-    template: "template1.html",
-    ttl: 5000
-}
-images_path = "/resources/"
-var messages = [];
-var messagesLength=1;
-var findMessages = function(screenNum,db, callback) {
-
-    var cursor =db.collection('messages').find({"message.screen":screenNum});
-    cursor.each(function(err, doc) {
-        assert.equal(err, null);
-        if (doc != null) {
-           // console.log(doc.message)
-            messages.push(doc.message);
-            //console.log(JSON.parse(doc).getAttributeNode("message").toString());
-            //console.dir(doc);
-        }
-    });
-    callback();
+var noMessage = {
+    "text": ["no message to show"],
+    "template": "template1.html",
+    "screens": {
+        "1": {ttl: 5000},
+        "2": {ttl: 5000},
+        "3": {ttl: 5000}
+    }
 };
 
+var images_path = "/resources/";
+var db;
 
+var findMessages = function (screenNum, db, callback) {
+    // Sets all the needed variables
+    var now = new Date();
+    var current_date = now.getFullYear() + '-' + padNumber(now.getMonth() + 1) + '-' + padNumber(now.getDate());
+    var day_of_week = now.getDay() + 1;
+    var time = padNumber(now.getHours()) + ':' + padNumber(now.getMinutes());
+    var screen_field = 'screens.' + screenNum.toString();
+    var start_date_field = screen_field + '.date_to_show.from';
+    var end_date_field = screen_field + '.date_to_show.to';
+    var times_to_show_field = screen_field + '.times_to_show';
 
+    // Prepare query
+    var query = {};
+    query[screen_field] = {'$exists': true};
+    query[start_date_field] = {'$lte': current_date};
+    query[end_date_field] = {'$gt': current_date};
+    query[times_to_show_field] = {
+        '$elemMatch': {
+            'day': day_of_week.toString(),
+            'time.from': {'$lte': time},
+            'time.to': {'$gt': time}
+        }
+    };
 
-var screen_status = {};
+    db.collection('messages').find(query, function (err, cursor) {
+        cursor.toArray(function (err, items) {
+            console.log("length:" + items.length);
+            callback(items);
+        });
+        ;
+    });
+};
+
+app.get("/init", function (request, response) {
+        console.log('check')
+        response.sendFile(__dirname + "/MainScreen.html");
+    }
+);
 
 app.get("/screen=1", function (request, response) {
-        // If Screen already requested messages
-        if ("1" in screen_status) {
-            screen_status["1"] = (screen_status["1"] + 1) % messagesLength;
-            response.json(showNext(1));
-        }
-        // If Screen didnt already requested messages
-        else {
-            screen_status["1"] = -1;
+        if (request.query.init == null) {
             response.sendFile(__dirname + "/MainScreen.html");
+        }
+        else {
+            // If Screen already requested messages
+            showNext(1, function (next_message) {
+                response.json(next_message);
+            });
         }
     }
 );
 
 app.get("/screen=2", function (request, response) {
-        // If Screen already requested messages
-        if ("2" in screen_status) {
-            screen_status["2"] = (screen_status["2"] + 1) % messages.length;
-            response.json(showNext(2));
-        }
-        // If Screen didnt already requested messages
-        else {
-            screen_status["2"] = -1;
+        if (request.query.init == null) {
             response.sendFile(__dirname + "/MainScreen.html");
+        }
+        else {
+            // If Screen already requested messages
+            showNext(2, function (next_message) {
+                response.json(next_message);
+            });
         }
     }
 );
 
 app.get("/screen=3", function (request, response) {
-        // If Screen already requested messages
-        if ("3" in screen_status) {
-            screen_status["3"] = (screen_status["3"] + 1) % messages.length;
-            response.json(showNext(3));
-        }
-        // If Screen didnt already requested messages
-        else {
-            screen_status["3"] = -1;
+        if (request.query.init == null) {
             response.sendFile(__dirname + "/MainScreen.html");
+        }
+        else {
+            // If Screen already requested messages
+            showNext(3, function (next_message) {
+                response.json(next_message);
+            });
         }
     }
 );
@@ -84,82 +105,32 @@ app.get("/", function (request, response) {
     response.sendFile(__dirname + "/NoScreenDefined.html")
 });
 
-app.listen(8080);
+MongoClient.connect(url, function (err, connection) {
+        db = connection;
+        app.listen(8080);
+    }
+);
 
-function isDateBetween(dateFrom, dateTo, dateCheck) {
-    var d1 = dateFrom.split(".");
-    var d2 = dateTo.split(".");
-    var c = dateCheck.split(".");
+function padNumber(number) {
 
-    var from = new Date(d1[2], parseInt(d1[1]) - 1, d1[0]);  // -1 because months are from 0 to 11
-    var to = new Date(d2[2], parseInt(d2[1]) - 1, d2[0]);
-    var check = new Date(c[2], parseInt(c[1]) - 1, c[0]);
-    return (check >= from && check <= to)
+    if (number >= 0 && number < 10) {
+        padded_number = '0' + number;
+    }
+    else {
+        padded_number = number;
+    }
+
+    return padded_number;
 }
 
-function showNext(screen) {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
-    var yyyy = today.getFullYear();
-    var fullDate = dd + "." + mm + "." + yyyy;
-    var hour = today.getHours() + ":" + "00";
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    var day = weekday[today.getDay()];
+function showNext(screen, callback) {
+    findMessages(screen, db, function (messages) {
+        var next_message = noMessage;
 
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        findMessages(screen,db, function() {
-            for (var i in messages) {
+        if (messages.length > 0) {
+            next_message = messages[Math.floor(Math.random() * messages.length)];
+        }
 
-                message_index = (parseInt(i) + parseInt(screen_status[screen])) % messagesLength;
-
-                for (var screen_index in messages[message_index]) {
-                    if (messages[message_index][screen_index]["screen"] == screen) {
-                        for (var times_index in messages[message_index][screen_index]["config"]["times_to_show"]) {
-                            if (messages[message_index][screen_index]["config"]["times_to_show"][times_index]["day"] == day &&
-                                isDateBetween(messages[message_index][screen_index]["config"]["date_to_show"][0], messages[message_index][screen_index]["config"]["date_to_show"][1], fullDate)) {
-                                var fromHour = messages[message_index][screen_index]["config"]["times_to_show"][times_index]["time"][0];
-                                var toHour = messages[message_index][screen_index]["config"]["times_to_show"][times_index]["time"][1];
-                                var fromDiff = ( new Date("1970-1-1 " + hour + ":00") - new Date("1970-1-1 " + fromHour) ) / 1000 / 60 / 60;
-                                var toDIff = ( new Date("1970-1-1 " + toHour) - new Date("1970-1-1 " + hour + ":00") ) / 1000 / 60 / 60;
-
-                                if (fromDiff >= 0 && toDIff >= 0) {
-                                    screen_status[screen] = message_index;
-
-                                    return messages[message_index][screen_index]["config"];
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            if(messages.length!=0){
-            messagesLength=messages.length;
-            }
-            else
-            {
-                messagesLength=1;
-            }
-            console.log(messages.length);
-            messages=[];
-            db.close();
-        });
+        callback(next_message);
     });
-
-  //  messages.push("",function () {
-    //    console.log("wow");
-
-    //});
-
-
- //   function(){console.log("bla")},100);
-    return noMessage;
 }
