13157236934cecd390d003c9a1aacd99ac09577c
guyra <guyra>
2017-05-19 21:19:18 +0100

    run.bat updated to run mongo - needs to be transformed to windows

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-start "" "c:\program files (x86)\google\chrome\application\chrome.exe" --new-window "http://localhost:8080/screen=2"
+sudo service mongod start
+node initDB.js
 node server.js
+start "" "c:\program files (x86)\google\chrome\application\chrome.exe" --new-window "http://localhost:8080/screen=2"
